52341e2ece903fe7fc61cfefaad741d18d29255d
kaikybrofc <kaikygomesribeiroof@gmail.com>
2026-04-13 01:28:52 +0000

    adiciona configuração de opções de linguagem para arquivos CommonJS no ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,20 @@ export default tseslint.config(
   {
     ignores: ['dist/**', 'node_modules/**', 'logs/**', 'data/**'],
   },
+  {
+    files: ['**/*.cjs'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: {
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        exports: 'readonly',
+        module: 'readonly',
+        process: 'readonly',
+        require: 'readonly',
+      },
+    },
+  },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
